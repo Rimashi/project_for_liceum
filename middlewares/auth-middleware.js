8e9c463d357ea2e95d9ee3ae9c4124799d9dd3c8
Rimashi <88615946+Rimashi@users.noreply.github.com>
2023-10-22 18:20:40 +0700

    Update auth-middleware.js

--- middlewares/auth-middleware.js
+++ middlewares/auth-middleware.js
@@ -6,30 +6,18 @@ module.exports = function (req, res, next) {
     try {
         const access = req.cookies.accessToken;
         if (!access) {
-            return res.redirect('/index');
+            return res.redirect('/');
         }
         const userData = tokenService.validateAccessToken(access);
         if (!userData) {
-            return res.redirect('/index');
+            return res.redirect('/');
         }
-        //console.log(userData);
-        // switch (userData.status) {
-        //     case "student":
-        //         return res.redirect("/student");
-        //     case "leader":
-        //         return res.redirect('/leader');
-        //     case "admin":
-        //         return res.redirect('/admin');
-        //     default:
-        //         console.log("status is not defind");
-        //         return res.redirect('/index');
-        // }
 
         req.user = userData;
-        console.log("auth-mid");
+        //console.log("auth-mid");
         return next();
     } catch (e) {
         console.log("wrong authorization")
-        res.redirect('/index');
+        res.redirect('/');
     }
 }
