dd818d38d81876f76da2ea33c1896ea475bf3fa1
Rimashi <88615946+Rimashi@users.noreply.github.com>
2023-12-25 03:08:38 +0700

    Add files via upload

---
+++ dtos/api-error.js
@@ -0,0 +1,18 @@
+module.exports = class ApiError extends Error {
+    status;
+    errors;
+
+    constructor(status, message, errors = []) {
+        super(message);
+        this.status = status;
+        this.errors = errors;
+    }
+
+    static BadRequest(message, errors = []) {
+        return new ApiError(400, message, errors);
+    }
+
+    static MaybeServerProblem(message, errors = []) {
+        return new ApiError(500, message, errors);
+    }
+}
