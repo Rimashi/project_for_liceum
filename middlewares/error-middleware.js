e8bc69801d5c457248242f272db533395691c5e7
Rimashi <88615946+Rimashi@users.noreply.github.com>
2024-03-08 12:10:51 +0700

    Update error-middleware.js

--- middlewares/error-middleware.js
+++ middlewares/error-middleware.js
@@ -1,12 +1,11 @@
 const apiError = require('../dtos/api-error');
-const path = require("path");
 
-const createPath = (page) => path.resolve(__dirname, '../views', `${page}.ejs`);
+
 module.exports = function (err, req, res, next) {
-    // console.log(err);
+    console.error("ERROR: " + err);
 
     if (err instanceof apiError) {
         return res.status(err.status).redirect(`/error?status=${err.status}&message=${encodeURIComponent(err.message)}`);
     }
-    return res.status(500).redirect(`/error?status=${500}&message=${encodeURIComponent("Произошла непредвиденная ошибка на сервере" + err.message)}`);
+    return res.status(500).redirect(`/error?status=${500}&message=${encodeURIComponent("Произошла непредвиденная ошибка на сервере")}`);
 }
